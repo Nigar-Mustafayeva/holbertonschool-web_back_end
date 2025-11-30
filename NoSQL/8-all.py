8d0faf3604e3f7f16e94062de101782985db4f10
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2025-11-30 16:05:07 +0000

    .

---
+++ NoSQL/8-all.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" 8-all """
+
+def list_all(mongo_collection):
+    """
+    List all documents in a MongoDB collection
+    Args:
+        mongo_collection: pymongo collection object
+    Returns:
+        List of documents, or empty list if none
+    """
+    if mongo_collection is None:
+        return []
+    return list(mongo_collection.find())
