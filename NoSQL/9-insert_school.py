075a5bc5382e6efd1d868d7499325188d95efc6f
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2025-11-30 16:06:57 +0000

    .

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" 9-insert_school """
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Insert a new document into a MongoDB collection
+    Args:
+        mongo_collection: pymongo collection object
+        **kwargs: key/value pairs representing the document fields
+    Returns
+        The _id of the newly inserted document
+    """
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
